1976af47963dfab24368371316f45a97219e5b20
annasybingco <sybingcoanna@gmail.com>
2025-02-13 10:40:39 -0500

    update variables to include light and dark mode

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,23 +1,28 @@
-import '../../styles/_typography.scss'
-import '../Homepage/Homepage.scss'
-import Burger from "../../assets/images/original.png"
+import "../../styles/_typography.scss";
+import "../Homepage/Homepage.scss";
+import Burger from "../../assets/images/original.png";
+import useTheme from "../../../components/Theme/theme.jsx";
+import "../../styles/_variables.scss";
+
+import React from "react";
+
+export function ThemeToggle() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button onClick={toggleTheme}>
+      {theme === "light" ? "🌙 Dark Mode" : "☀️ Light Mode"}
+    </button>
+  );
+}
 
 function Homepage() {
   return (
-    <section className='homepage'>
-      <h2 className="heading-2">Burgers</h2>
-      <div className='menu'>
-<ul className='menu__item'>
-    <li className='menu__title h4'>Original Burger</li>
-    <li className='menu__description body-small'>Griddled sweet onion, American cheese, house pickles, drop sauce, toasted potato roll.</li>
-    <li className='menu__price body'>$8.50</li>
-</ul>
-<div>
-    <img src={Burger} alt="original-burger" className='menu__image' />
-</div>
-      </div>
-    </section>
-  )
+    <div>
+      <ThemeToggle />
+      <h1>Burger Drops</h1>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
