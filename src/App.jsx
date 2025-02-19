f9118f81d4b35d2f7525565ae0fae8cc008b363f
annasybingco <sybingcoanna@gmail.com>
2025-02-19 09:20:40 -0500

    refactor theme component into using useContext to pass down theme through provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { createContext, useContext, useState } from "react";
+import React from "react";
+import FunctionContextComponent from "./Functions/FunctionContextComponent.jsx";
 import Homepage from "./pages/Homepage/Homepage";
 import Details from "./pages/Details/Details";
 import Cart from "./pages/Cart/Cart";
+import { ThemeProvider } from "./Context/ThemeContext.jsx";
 
-const shoppingCartContext = createContext();
 function App() {
-  const [shoppingCart, setShoppingCart] = useState([]); //initialize shopping cart
   return (
-    <shoppingCartContext.Provider value={{ shoppingCart, setShoppingCart }}>
+    <ThemeProvider>
+      <FunctionContextComponent />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
@@ -16,7 +17,7 @@ function App() {
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
-    </shoppingCartContext.Provider>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,6 +1,5 @@
 import "../../styles/_typography.scss";
 import "./Homepage.scss";
-import useTheme from "../../components/Theme/Theme.jsx";
 import "../../styles/_variables.scss";
 import Header from "../../components/Header/Header.jsx";
 
@@ -8,23 +7,11 @@ import React from "react";
 import MenuItems from "../../components/MenuItems/MenuItems.jsx";
 import Navigation from "../../components/Navigation/Navigation.jsx";
 
-//needs to move to profile page
-export function ThemeToggle() {
-  const { theme, toggleTheme } = useTheme();
-
-  return (
-    <button onClick={toggleTheme}>
-      {theme === "light" ? "Dark Mode" : "Light Mode"}
-    </button>
-  );
-}
-
 function Homepage() {
   return (
     <div className="page">
       <Header />
       <Navigation />
-      <ThemeToggle />
       <MenuItems />
     </div>
   );
