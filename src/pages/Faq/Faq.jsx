a5b647f49baa87c2e868bde8f6cf93484082a8e7
annasybingco <sybingcoanna@gmail.com>
2025-02-19 22:25:25 -0500

    add faq page information

--- src/pages/Faq/Faq.jsx
+++ src/pages/Faq/Faq.jsx
@@ -1,16 +1,54 @@
-// import FAQ from "./FAQ";
-
 import Navigation from "../../components/Navigation/Navigation.jsx";
-
-import React from "react";
+import "../Faq/Faq.scss";
+import add from "../../assets/icons/add.svg";
+import React, { useState } from "react";
 
 function Faq() {
+  const [selected, setSelected] = useState(null);
+
+  const toggle = (i) => {
+    if (selected === i) {
+      return setSelected(null);
+    }
+
+    setSelected(i);
+  };
   return (
-    <div>
+    <div className="wrapper">
       <Navigation />
-      <h1>This is my FAQ</h1>
+      <h4>Ordering</h4>
+      <div className="accordion">
+        {data.map((item, i) => (
+          <div className="item">
+            <div className="title" onClick={() => toggle(i)}>
+              <p>{item.question}</p>
+              <span>{selected === i ? "-" : "+"}</span>
+            </div>
+
+            {selected === i && <div className="content">{item.answer}</div>}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+
+const data = [
+  {
+    question: "How can I place an order?",
+    answer:
+      "To place an order, you have many options. Drop by the shop to order in-person during our regular hours, order ahead for pick-up on Ritual, our app or for delivery on Uber Eats and Doordash.",
+  },
+  {
+    question: "Can I order over the phone?",
+    answer:
+      "Phone orders are not possible at this time as we cannot process a transaction for your order via text or phone.",
+  },
+  {
+    question: "Do you offer catering?",
+    answer:
+      "Yes! Burger Drops specializes in both on-site private catering and large group orders for your office or next celebration.",
+  },
+];
 
 export default Faq;
