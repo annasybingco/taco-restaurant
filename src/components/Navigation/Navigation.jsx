2bfea8ad402036256e08be358e44d9293ed70ce5
annasybingco <sybingcoanna@gmail.com>
2025-02-20 13:23:38 -0500

    update svg and button color

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,33 +1,35 @@
-import HomeLogo from "../SVG/HomeLogo";
+import { Link, useLocation } from "react-router-dom";
 import RewardsLogo from "../SVG/RewardsLogo";
 import ProfileLogo from "../SVG/ProfileLogo";
 import FaqLogo from "../SVG/FaqLogo";
-import { Link } from "react-router-dom";
+import HomeLogo from "../SVG/HomeLogo";
 import "../Navigation/Navigation.scss";
 
 function Navigation() {
+  const location = useLocation();
+
   return (
     <section className="navigation">
       <Link to="/">
-        <div className="navigation__item">
+        <div className={location.pathname === "/" ? "active" : ""}>
           <HomeLogo />
           <p>Menu</p>
         </div>
       </Link>
       <Link to="/rewards">
-        <div className="navigation__item">
+        <div className={location.pathname === "/rewards" ? "active" : ""}>
           <RewardsLogo />
           <p>Rewards</p>
         </div>
       </Link>
       <Link to="/profile">
-        <div className="navigation__item">
+        <div className={location.pathname === "/profile" ? "active" : ""}>
           <ProfileLogo />
           <p>Profile</p>
         </div>
       </Link>
       <Link to="/faq">
-        <div className="navigation__item">
+        <div className={location.pathname === "/faq" ? "active" : ""}>
           <FaqLogo />
           <p>FAQ</p>
         </div>
