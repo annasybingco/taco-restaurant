5bfd7238159303e8b4a6b83fff2e64f8c751e639
annasybingco <sybingcoanna@gmail.com>
2025-02-23 10:36:05 -0500

    refactor menu items detail page for cleaner code

--- src/components/MenuItems/MenuItems.jsx
+++ src/components/MenuItems/MenuItems.jsx
@@ -23,14 +23,14 @@ function MenuItems() {
     <div className="page">
       <ul className="menu">
         {menu.map((item) => (
-          <li className="menu__item" key={item.id}>
-            <Link to={`/menu/${item.id}`} className="menu__square">
+          <li className="" key={item.id}>
+            <Link to={`/menu/${item.id}`} className="menu__item">
               <div className="menu__details">
                 <h4>{item.title}</h4>
                 <p className="body-small">{item.description}</p>
                 <p>${item.price}</p>
               </div>
-              <div className="menu__photo">
+              <div>
                 <img
                   src={item.photo}
                   alt={item.title}
